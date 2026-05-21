8fa0fdf570d67b89bf6097c9ee2c04c58fcf07ba
jpdealba <jpdeaty1207@gmail.com>
2026-05-20 19:03:37 -0600

    Reduce database pool size in development configuration from 75 to 45 for optimized resource management.

--- global_payroll/config/dev.exs
+++ global_payroll/config/dev.exs
@@ -9,7 +9,7 @@ config :global_payroll, GlobalPayroll.Repo,
   database: "global_payroll_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 75
+  pool_size: 45
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
